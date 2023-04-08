7677e7809d9f0a41f3d3c45b45804bf9ac84403b
Artem <gil.artem@ya.ru>
2023-04-08 22:34:48 +0300

    I added test with str.length for next function.

--- Program.cs
+++ Program.cs
@@ -3,28 +3,29 @@
 // либо задать на старте выполнения алгоритма. 
 // При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
-void Print(double[] arr)
+void Print(string[] arr)
 {
     int row = arr.GetLength(0);
 
     for (int i = 0; i < row; i++)
     {
         Console.Write($"{arr[i]}  ");
+        Console.Write($"-{arr[i].Length}-  ");
     }
     Console.WriteLine();
 }
 
-double[] MassNums(int row)
+string[] MassRead(int row)
 {
-    double[] arr = new double[row];
+    string[] arr = new string[row];
 
     for (int i = 0; i < row; i++)
-            arr[i] = int.Parse(Console.ReadLine()!);
+            arr[i] = Console.ReadLine()!;
 
     return arr;
 }
 
-int num_row = int.Parse(Console.ReadLine()!);
+int num_size = int.Parse(Console.ReadLine()!);
 
-double[] mass = MassNums(num_row);
+string[] mass = MassRead(num_size);
 Print(mass);
